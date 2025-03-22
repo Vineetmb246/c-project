c86dd3394a058c89621ccab960ba193aa6678ab5
Vineetmb246 <vineetmb246@gmail.com>
2025-03-22 16:17:37 +0530

    Update test_math.py

--- test_math.py
+++ test_math.py
@@ -1,5 +1,7 @@
-def add(a,b);
-return a+b
+# Function to add two numbers
+def add(a, b):
+    return a + b
 
-def test_add();
-assert add(2,3) ==5
+# Test function for add()
+def test_add():
+    assert add(2, 3) == 5
