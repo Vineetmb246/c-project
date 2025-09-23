6caec65e61f5b4c72c2bf50f054f958dc5a09223
Cloud User <ec2-user@ip-172-31-35-36.ap-south-1.compute.internal>
2025-09-23 13:02:58 +0000

    Fix: Added function prototypes and return type in main.c

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
-main()
-{
-	big();
-	fact();
-	rev();
+#include <stdio.h>
+
+// Function prototypes
+void big();
+void fact();
+void rev();
+
+int main() {
+    big();
+    fact();
+    rev();
+    return 0;
 }
+
